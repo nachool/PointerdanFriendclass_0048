0f6fbea5c694c4bc9cbb8cbd3700da0a970b8bbd
nachool <nachma.olivia1394@gmail.com>
2024-05-08 11:51:47 +0700

    memanggil elmeen

--- pointer2/pointer2.cpp
+++ pointer2/pointer2.cpp
@@ -5,12 +5,16 @@ class mahasiswa {
 public: 
     int nim;
     void showNim() {
-
+        cout << "No Induk = " << nim << endl;
     }
 };
 
 int main()
 {
-    
+    mahasiswa* mhs = new mahasiswa{ 1 };
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
 }
 
